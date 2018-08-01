9f9eb2af3bb331acd99ae986f85e3158163050c9
FalconTube <yannic.falke@gmail.com>
2018-08-01 13:56:31 +0200

    Added more functionality to Korad

--- korad_usage.py
+++ korad_usage.py
@@ -7,23 +7,17 @@ class UseKorad(KoradSerial):
     def __init__(self):
         try:
             self.korad = KoradSerial('COM5')
+            self.channel = self.korad.channels[0]
             self.korad.output.on()
         except:
             print('Cannot turn on the Korad. Exiting... ')
             sys.exit()
-        self.channel = self.korad.channels[0]
-        testrange = self.define_range()
-        try:
-            self.run_range(testrange)
-        except KeyboardInterrupt:
-            self.finish()
-        self.finish()
         
-
     def define_range(self):
         start = float(input('Insert STARTing current in Amp: '))
         end = float(input('Insert ENDing current in Amp: '))
-        ptime = float(input('Insert time, that the process shall take in MINutes: '))
+        ptime = float(input('Insert time, that the process shall take'
+        + ' in MINutes: '))
         ptime *= 60 # Conversion so seconds
         steps = np.linspace(start, end, ptime)
         return steps
@@ -37,54 +31,32 @@ class UseKorad(KoradSerial):
             self.channel.current = i
             time.sleep(0.98)
     
+    def run_amount(self):
+        amount = int(input("How many ranges do you want to define? ") or 1)
+        if amount == 1:
+            testrange = self.define_range()
+            self.run_range(testrange)
+        else:
+            runlist = []
+            for n in range(amount):
+                print("\nInput run number {}".format(n))
+                testrange = self.define_range()
+                runlist.append(testrange)
+            
+            for n in runlist:
+                self.run_range(n)
+
     def finish(self):
         print('Turning off output and closing device.')
         self.korad.output.off()
         self.close()
 
 
-# class KoradSerialTest(TestCase):
-#     def __init__(self):
-#         self.setUp()
-#         self.test_channel1()
-
-#     def setUp(self):
-#         self.device = KoradSerial('COMX', True)
-#         self.overrideSkippedTests = False
-
-#     def test_channel1(self):
-#         """ Test Channel 1's functionality.
-#         This test assumes a small load (perhaps 100 ohm) is on the power supply so a small amount of current is drawn.
-#         """
-#         channel = self.device.channels[0]
-
-#         # Turn off output and ensure that it's reading zeroes.
-#         self.device.output.off()
-#         time.sleep(1)
-#         self.assertEqual(0, channel.output_voltage)
-#         self.assertEqual(0, channel.output_current)
-
-#         # Set the current and voltage and ensure it's reporting back correctly.
-#         channel.voltage = 1.34
-#         channel.current = 0.1234
-#         self.assertAlmostEqual(1.34, channel.voltage, 2)
-#         self.assertAlmostEqual(0.1234, channel.current, 4)
-
-#         # Set a different current and voltage to ensure that we're not reading old data.
-#         channel.voltage = 1.30
-#         channel.current = 0.123
-#         self.assertAlmostEqual(1.30, channel.voltage, 2)
-#         self.assertAlmostEqual(0.123, channel.current, 3)
-
-#         # Turn on the output and ensure that current is flowing across the small load.
-#         self.device.output.on()
-#         time.sleep(1)
-#         self.assertAlmostEqual(1.30, channel.output_voltage, 2)
-#         self.assertLess(0, channel.output_current)
-
-#         self.device.output.off()
-#     # channel.voltage = 0.1
-#     # print(channel.output_voltage)
-
 if __name__ == "__main__":
     k = UseKorad()
+    try:
+        k.run_amount()
+    except KeyboardInterrupt:
+        k.finish()
+    k.finish()
+    
