3cf22129bfc9fe84b15655d7a73ee5056fd169c6
FalconTube <yannic.falke@gmail.com>
2018-09-17 13:36:49 +0200

    Added Caching system and made necessary changes to other files

---
+++ Classes/caching_system.py
@@ -0,0 +1,60 @@
+import time
+import numpy as np
+import sys
+import os
+
+
+class Caching_System():
+    def __init__(self):
+        self._initial_caching = 0
+        self.cache_counter = 0
+        self.cachefile = '.own_cache'
+        self.ask_for_cache()
+        if os.path.isfile(self.cachefile):
+            self.cached_vals = self.read_cache()
+        self.use_cache = False
+
+    def ask_for_cache(self):
+        usage = input(
+            'Would you like to use the cached values' +
+            ' from the last session? (y/n)' or 'n')
+        if usage.lower().strip() == 'y':
+            print('Okay, using cached values...')
+            self.use_cache = True
+        else:
+            print('Will not use the cache.')
+            self.use_cache = False
+
+    def write_cache(self, argument):
+        if self._initial_caching == 0:
+            open(self.cachefile, 'w').close()  # Clear cachefile
+
+        with open(self.cachefile, 'a') as f:
+            f.write('{}\n'.format(argument))
+
+    def read_cache(self):
+        values = []
+        with open(self.cachefile, 'r') as f:
+            for line in f:
+                stripline = line.rstrip()
+                values.append(stripline)
+        return values
+
+    def cache_input(self, input_arg, default=''):
+        ''' Use this instead of standard input if you want to cache. '''
+        if self.use_cache == False:
+            outval = input(input_arg)
+        else:
+            outval = self.cached_vals[self.cache_counter]
+            print('{} {}'.format(input_arg, outval))
+        if outval == '':
+            outval = default
+        self.write_cache(outval)
+        self._initial_caching += 1
+        self.cache_counter += 1
+        return outval
+
+
+if __name__ == '__main__':
+    print('Calling this caching class directly is of no use. Exiting...')
+    sys.exit()
--- gatesweep.py
+++ gatesweep.py
@@ -14,11 +14,11 @@ class Gatesweep(Measurement):
         self.ask_parameters()
         self.wait_max = self.ask_wait_at_maxvals()
         savestring = \
-        '# gatevoltage(V), temp(K), voltage(V), current(A), R_4pt(W)'
+            '# gatevoltage(V), temp(K), voltage(V), current(A), R_4pt(W)'
         self.create_savefile(savestring)
         self.init_ramp_parameters()
         try:
-            self.start_gatesweep()            
+            self.start_gatesweep()
         except KeyboardInterrupt:
             self.gate.set_gatevoltage(0)
             self.finish_measurement()
@@ -33,17 +33,17 @@ class Gatesweep(Measurement):
     def ramp_gatevoltage(self):
         ''' Increments the gatevoltage and finishes the measurement '''
         if self.gatevoltage < self.maxvoltage and \
-        self.gatevoltage >= self.lastvoltage:
+                self.gatevoltage >= self.lastvoltage:
             self.lastvoltage = self.gatevoltage
             self.gatevoltage += self.stepsize
         elif self.gatevoltage == self.maxvoltage:
             if self.wait_max:
-                time.sleep(30) # wait at max voltage
+                time.sleep(30)  # wait at max voltage
             self.lastvoltage = self.gatevoltage
             self.gatevoltage -= self.stepsize
             self.maxcounter += 1
         elif self.gatevoltage > self.minvoltage and \
-        self.gatevoltage < self.lastvoltage:
+                self.gatevoltage < self.lastvoltage:
             self.lastvoltage = self.gatevoltage
             self.gatevoltage -= self.stepsize
         elif self.gatevoltage == self.minvoltage:
@@ -53,19 +53,19 @@ class Gatesweep(Measurement):
         if self.maxvoltage >= 0 and self.minvoltage >= 0:
             if self.maxcounter == 2:
                 self.finish_measurement()
-        if self.gatevoltage == 0 :
+        if self.gatevoltage == 0:
             self.finishedcounter += 1
             if self.finishedcounter == 2:
                 self.finish_measurement()
 
-    def benchmark_slope(self, x,y):
+    def benchmark_slope(self, x, y):
         """ Uses first few measurement points to generate initial slope.
-            CURRENTLY NOT IN USE   
+            CURRENTLY NOT IN USE
         """
         x = np.asarray(x)
         y = np.asarray(y)
         if len(x) > 5:
-            slope, b = np.polyfit(x[:5],y[:5],1) 
+            slope, b = np.polyfit(x[:5], y[:5], 1)
             plt.plot(x[:5], self.linear(x[:5], slope, b), 'g-')
             return slope
         else:
@@ -78,9 +78,9 @@ class Gatesweep(Measurement):
     def check_slope(self, x, y, benchslope, saveval):
         """ Checks, if the slope exceeds a given value. If this is the case,
             it stops the program and prompts user to take action.
-            CURRENTLY NOT IN USE   
+            CURRENTLY NOT IN USE
         """
-        slope, b = np.polyfit(x[-2:],y[-2:],1)
+        slope, b = np.polyfit(x[-2:], y[-2:], 1)
         if slope > saveval*benchslope:
             print("Measured slope exceeds save value. Stopping Program...")
             # plt.plot(x[-2:], linear(x[-2:], slope, b), 'r-')
@@ -90,29 +90,31 @@ class Gatesweep(Measurement):
 
     def ask_parameters(self):
         ''' Define measurement range based on CLI user input '''
-        self.maxvoltage = float(input('Set MAXvoltage (standard is 40, if not defined): ') or 40)
-        self.minvoltage = float(input('Set MINvoltage (standard is -40, if not defined): ') or -40)
-        self.stepsize = float(input('Set stepsize (standard is 0.5, if not defined): ') or 0.5)
-        self.waittime = float(input('Set wait time after each step (standard is 10 sec, if not defined): ') or 10)
+        self.maxvoltage = float(self._cache.cache_input(
+            'Set MAXvoltage (standard is 40, if not defined): ', 40))
+        self.minvoltage = float(self._cache.cache_input(
+            'Set MINvoltage (standard is -40, if not defined): ', -40))
+        self.stepsize = float(self._cache.cache_input(
+            'Set stepsize (standard is 0.5, if not defined): ', 0.5))
+        self.waittime = float(self._cache.cache_input(
+            'Set wait time after each step (standard is 10 sec, if not defined): ', 10))
         # Calculate total time, that measurement will take in SEC
         if self.minvoltage < 0:
-            total_time_sec =\
-             ((self.maxvoltage/self.stepsize) +\
-             (self.maxvoltage - self.minvoltage)/self.stepsize +\
-             (abs(self.minvoltage)/self.stepsize)
-             )*self.waittime
+            total_time_sec = ((self.maxvoltage/self.stepsize) +
+                              (self.maxvoltage - self.minvoltage)/self.stepsize +
+                              (abs(self.minvoltage)/self.stepsize)
+                              )*self.waittime
         else:
-            total_time_sec =\
-             ((self.maxvoltage/self.stepsize) +\
-             (self.maxvoltage - self.minvoltage)/self.stepsize\
-             )*self.waittime 
-        total_time_min = round(total_time_sec/60,2)
+            total_time_sec = ((self.maxvoltage/self.stepsize) +
+                              (self.maxvoltage - self.minvoltage)/self.stepsize
+                              )*self.waittime
+        total_time_min = round(total_time_sec/60, 2)
         print('This measurement will take {} minutes.'.format(total_time_min))
-
 
     def ask_wait_at_maxvals(self):
         ''' Should max voltage be kept for certain time? '''
-        wait_max = str(input('Do you want to hold the maximum voltage for 30 sec? (y/n): ') or 'n').lower()
+        wait_max = str(self._cache.cache_input(
+            'Do you want to hold the maximum voltage for 30 sec? (y/n): ', 'n').lower())
         if wait_max == 'y':
             print('I will wait at Gatevoltage = {}'.format(self.maxvoltage))
             return True
@@ -125,20 +127,14 @@ class Gatesweep(Measurement):
         y = []
         r = []
         gc = []
-        fig = plt.figure()
-        ax = fig.add_subplot(211)
-        ax1 = fig.add_subplot(212)
-        ax1.set_xlabel("Gatevoltage [V]")
-        ax.set_ylabel("Resistance [Ohm]")
-        ax1.set_ylabel("Gatecurrent [A]")
         while 1:
             # Set gatevoltage and measure values
             print('Gatevoltage = {}'.format(self.gatevoltage))
-            self.gate.set_gatevoltage(self.gatevoltage)  
+            self.gate.set_gatevoltage(self.gatevoltage)
             time.sleep(self.waittime)
-            meterV = self.meter.read_voltage() 
-            meterI = self.meter.read_current() 
-            temp=self.lakeshore.read_temp()
+            meterV = self.meter.read_voltage()
+            meterI = self.meter.read_current()
+            temp = self.lakeshore.read_temp()
             gatecurrent = self.gate.read_current()
 
             # Plot values in real time
@@ -147,20 +143,19 @@ class Gatesweep(Measurement):
             y.append(meterV)
             r.append(meterV/meterI)
             gc.append(gatecurrent)
-
-            ax.plot(x, r, 'k.')
-            ax1.plot(x, gc, 'k.')
-            plt.draw()
-            plt.pause(0.01)
+            self.fast_plotter(x, r, ax_num=2, ax_pos=0,
+                              labels=("Gatevoltage [V]", "Resistance [Ohm]"))
+            self.fast_plotter(x, gc, ax_num=2, ax_pos=1,
+                              labels=("Gatevoltage [V]", "Gatecurrent [A]"))
 
             # Write values to file
             writedict = {
-            'Gatevoltage' : self.gatevoltage,
-            'T': temp,
-            'V': meterV,
-            'I': meterI,
-            'R': meterV/meterI,
-            'I_gate': gatecurrent,
+                'Gatevoltage': self.gatevoltage,
+                'T': temp,
+                'V': meterV,
+                'I': meterI,
+                'R': meterV/meterI,
+                'I_gate': gatecurrent,
             }
             for i in writedict:
                 self.savefile.write('{} ,'.format(str(writedict[i]).strip()))
@@ -168,12 +163,10 @@ class Gatesweep(Measurement):
 
             # Set gatevoltage to next value
             self.ramp_gatevoltage()
-        #save figure file as png
+        # save figure file as png
         # figname = fn.split('.')[0] + '_mobility.png'
-        #plt.savefig(self.savename_png)
-            
+        # plt.savefig(self.savename_png)
+
+
 if __name__ == '__main__':
     gs = Gatesweep()
-
-
-    
